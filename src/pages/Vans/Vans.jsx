8536164e693f69815a392da90711c0da68747125
Ivan Marinski <noobdrew23@gmail.com>
2023-07-14 14:46:36 +0300

    fixed van element import

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import VanElement from "../components/VanElement";
+import VanElement from "/src/components/VanElement";
 export default function Vans() {
   const [vanData, setVanData] = useState([]);
 
