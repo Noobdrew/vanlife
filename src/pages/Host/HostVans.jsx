96e17dc699d4c2ebab272d961676deea5f1ea464
Ivan Marinski <noobdrew23@gmail.com>
2023-07-14 17:31:41 +0300

    host vans page done

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import HostVanElement from "../../components/HostVanElement";
+
 export default function HostVans() {
+  const [hostVans, setHostVans] = useState([]);
+  useEffect(() => {
+    fetch("/api/host/vans")
+      .then((resp) => resp.json())
+      .then((data) => setHostVans(data.vans));
+  }, []);
+  console.log(hostVans);
+  const hostVanElements = hostVans.map((van) => {
+    return (
+      <Link className="host-van-element" to={`/host/vans/${van.id}`}>
+        <HostVanElement
+          key={van.id}
+          img={van.imageUrl}
+          name={van.name}
+          price={van.price}
+          type={van.type}
+        />
+      </Link>
+    );
+  });
+
   return (
     <>
-      <h1>host vans page</h1>
+      <h1 className="host-van-title">Your listed vans</h1>
+      <div className="host-van-container"> {hostVanElements}</div>
     </>
   );
 }
